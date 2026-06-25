cfc9942fa69346157866fbc4f5fa5c3b278a586a
Cuong Tran <cuong.tran@horizon-alpha.com>
2026-06-25 08:57:56 +0200

    Add sigil types to config, reorganize assets into signs/modifiers folders, and apply code formatting

--- src/config.js
+++ src/config.js
@@ -1,50 +1,50 @@
-export const CANVAS_WIDTH = 500
-export const CANVAS_HEIGHT = 500
+export const CANVAS_WIDTH = 500;
+export const CANVAS_HEIGHT = 500;
 
-export const STROKE_COLOR = '#000000'
-export const STROKE_WIDTH = 2
-export const ERASER_WIDTH = 20
+export const STROKE_COLOR = "#000000";
+export const STROKE_WIDTH = 2;
+export const ERASER_WIDTH = 20;
 
-export const RUNES_PATH = '/assets/alphabet'
+export const RUNES_PATH = "/assets/alphabet/modifiers";
 
 export const RUNE_NAMES = [
-    'Aeriforms_Defined',
-    'Bend',
-    'Billowing',
-    'Binding',
-    'Bolt',
-    'Coil_Sign',
-    'Collection',
-    'Column',
-    'Convergence',
-    'Cooling',
-    'Crosshair',
-    'Crush',
-    'Dancing_Puppet',
-    'Diamond',
-    'Direction',
-    'Dispersion',
-    'Enlarge',
-    'Entwine',
-    'Float',
-    'Gather',
-    'Glaives',
-    'Levitation',
-    'Link_Sign',
-    'Orb',
-    'Pull',
-    'Purify_Sign',
-    'Radial',
-    'Rain',
-    'Repetition',
-    'Sign_of_concealment_redraw',
-    'Sign_of_projection_redraw',
-    'Sign_of_reflection_redraw',
-    'Sign_of_Wind',
-    'Solidify',
-    'Stasis_Sign',
-    'Weave',
-    'Window'
-]
+  "Aeriforms_Defined",
+  "Bend",
+  "Billowing",
+  "Binding",
+  "Bolt",
+  "Coil_Sign",
+  "Collection",
+  "Column",
+  "Convergence",
+  "Cooling",
+  "Crosshair",
+  "Crush",
+  "Dancing_Puppet",
+  "Diamond",
+  "Direction",
+  "Dispersion",
+  "Enlarge",
+  "Entwine",
+  "Float",
+  "Gather",
+  "Glaives",
+  "Levitation",
+  "Link_Sign",
+  "Orb",
+  "Pull",
+  "Purify_Sign",
+  "Radial",
+  "Rain",
+  "Repetition",
+  "Sign_of_concealment_redraw",
+  "Sign_of_projection_redraw",
+  "Sign_of_reflection_redraw",
+  "Sign_of_Wind",
+  "Solidify",
+  "Stasis_Sign",
+  "Weave",
+  "Window",
+];
 
-export const RUNE_COUNT = RUNE_NAMES.length
+export const RUNE_COUNT = RUNE_NAMES.length;
